4d8c3c086fc668248d0fb3b5702e39d023c18482
Ramone Robertson <ramonerobertson10@gmail.com>
2025-07-24 13:08:05 +0700

    Spring Boot Start

---
+++ src/main/java/com/mofumo/api/MofumoApplication.java
@@ -0,0 +1,13 @@
+package com.mofumo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofumoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MofumoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mofumo/api/MofumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mofumo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofumoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
