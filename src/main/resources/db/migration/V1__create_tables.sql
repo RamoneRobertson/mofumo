436f9861b99e408e6d9e3694357a40fe019b9c06
Ramone Robertson <ramonerobertson10@gmail.com>
2025-07-26 11:26:45 +0700

    Added providers table to init Flyway migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -6,7 +6,7 @@ create table users
     password          varchar(255)                                         not null,
     firstName         varchar(50)                                          not null,
     lastName          varchar(50)                                          not null,
-    phone             varchar(17)                                          not null,
+    phone             varchar(20)                                          not null,
     lineId            varchar(255)                                         null,
     ward              varchar(20)                                          null,
     userType          enum ('CUSTOMER', 'PROVIDER') default 'CUSTOMER'     not null,
@@ -35,8 +35,31 @@ create table providers
     created_at          timestamp default current_timestamp not null,
     constraint providers_users_id_fk
         foreign key (userId) references users (id)
+            on update cascade on delete restrict
+);
+
+create table pets_9
+(
+    id                      bigint auto_increment
+        primary key,
+    owner_id                bigint                                                                                      not null,
+    name                    varchar(25)                                                                                 not null,
+    species                 enum ('DOG', 'CAT', 'FISH', 'BIRD', 'HAMSTER', 'RABBIT', 'OTHER') default 'OTHER'           not null,
+    breed                   varchar(100)                                                                                null,
+    age_yr                  int                                                                                         null,
+    weight_kg               decimal(5, 2)                                                                               null,
+    medical_conditions      text                                                                                        null,
+    special_instruction     text                                                                                        null,
+    emergency_contact_name  varchar(100)                                                                                not null,
+    emergency_contact_phone varchar(20)                                                                                 not null,
+    active                  boolean                                                           default TRUE              not null,
+    createdAt               timestamp                                                         default current_timestamp not null,
+    constraint pets_9_users_id_fk
+        foreign key (owner_id) references users (id)
             on update cascade on delete cascade
 );
+
+
 
 
 
