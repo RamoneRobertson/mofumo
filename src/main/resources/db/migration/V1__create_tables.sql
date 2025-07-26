5c2f64afa6f247d059748e7e2a43867bf907a9fb
Ramone Robertson <ramonerobertson10@gmail.com>
2025-07-26 09:12:38 +0700

    Added providers table to initial migration script

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -17,5 +17,26 @@ create table users
     updatedAt         timestamp                                            not null
 );
 
+create table providers
+(
+    id                  bigint auto_increment
+        primary key,
+    userId              bigint                              not null,
+    business_name       varchar(100)                        not null,
+    description         text                                not null,
+    service_types       json                                not null,
+    languages_spoken    json                                not null,
+    service_areas       json                                not null,
+    mobile_service      boolean   default FALSE             null,
+    base_price          int                                 null,
+    verified            boolean   default FALSE             null,
+    accepts_new_clients boolean   default TRUE              null,
+    active              boolean   default TRUE              null,
+    created_at          timestamp default current_timestamp not null,
+    constraint providers_users_id_fk
+        foreign key (userId) references users (id)
+            on update cascade on delete cascade
+);
+
 
 
