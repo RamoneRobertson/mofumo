e680c94b267b81cdfd1517873a8a15c09972aebb
Ramone Robertson <ramonerobertson10@gmail.com>
2025-08-05 20:58:49 +0900

    Added AuthProvider and AuthManager to configure user authentication through UserDetails

--- src/main/java/com/mofumo/api/controllers/AuthController.java
+++ src/main/java/com/mofumo/api/controllers/AuthController.java
@@ -8,6 +8,9 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,23 +23,23 @@ public class AuthController {
   private final UserRepository userRepository;
   private final UserMapper userMapper;
   private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManager authenticationManager;
 
   @PostMapping("/login")
   public ResponseEntity<Void> login(
         @Valid @RequestBody LoginRequest request
   ){
-    var user = userRepository.findByEmail(request.getEmail()).orElseThrow(UserNotFoundException::new);
-
-    if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
+    authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+                  request.getEmail(),
+                  request.getPassword()
+          )
+    );
     return ResponseEntity.ok().build();
   }
 
-  @ExceptionHandler(UserNotFoundException.class)
-  public ResponseEntity<Map<String, String>> handleUserNotFoundException() {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-            Map.of("Error", "User not found.")
-    );
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<Map<String, String>> handleBadCredentialsException() {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 }
