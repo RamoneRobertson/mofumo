c529dcf13c69769014f387a1b569653e294cf2a8
Ramone Robertson <ramonerobertson10@gmail.com>
2025-08-25 16:08:35 +0900

    Adding comments for clarity

--- src/main/java/com/mofumo/api/services/JwtService.java
+++ src/main/java/com/mofumo/api/services/JwtService.java
@@ -6,10 +6,7 @@ import com.mofumo.api.repositories.UserRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
